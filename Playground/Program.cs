9b93872054d175c0a75d39227771737b850e4b54
Danny Bogers <dannybogers4@gmail.com>
2022-08-11 01:19:42 +0200

    More structured code, base reader on stream + internal buffer

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -13,177 +13,148 @@ public class Program
         var metaBytes = new Queue<byte>( //TODO: Convert to span
             File.ReadAllBytes(FilePath)
         );
-        
-        //Strip BOM header
-        // metaBytes.Pop();
-        // metaBytes.Pop();
-        // metaBytes.Pop();
 
-        //First value indicates the start of a dictionary
-        // metaBytes.Dequeue();
-
-        var values = new Dictionary<string, object>();
-        var currentKey = String.Empty;
-        var currentValue = (object)null;
-
-        var lengthIndication = new List<byte>();
-
-        // var type = PeekNextType(metaBytes);
-
+        using var reader = new BEncodingReader(File.OpenRead(FilePath));
+        var value = reader.Consume();
+    }
+    
+    
+    private class BEncodingReader : IDisposable
+    {
         //Attempt at implementing a basic BEncoding reader as per spec described in 
         //https://www.bittorrent.org/beps/bep_0003.html#bencoding
-        var metadata = ReadDictionary(metaBytes);
-
-        // while (type != BEncodingType.Unknown)
-        // {
-        //     if (type == BEncodingType.Dictionary)
-        //     {
-        //         //Consume
-        //         metaBytes.Dequeue();
-        //
-        //         //Read until we finished consuming our dictionary
-        //         while (PeekNextType(metaBytes) != BEncodingType.End)
-        //         {
-        //             var key = ReadString(metaBytes, ReadLength(metaBytes));
-        //             var value = ReadString(metaBytes, ReadLength(metaBytes));
-        //             
-        //             Console.WriteLine("key: " + key + " value: " + value);
-        //         }
-        //     }
-        //
-        //     break;
-        //     // type = PeekNextType(metaBytes);
-        // }
-
-
-        // var length = ReadLength(metaBytes);
-        // var str = ReadString(metaBytes, length);
-    }
-
-    private static List<object> ReadList(Queue<byte> metaBytes)
-    {
-        var list = new List<object>();
-        metaBytes.Dequeue(); //consume
-
-        while (PeekNextType(metaBytes) != BEncodingType.End)
-        {
-            list.Add(ReadValue(metaBytes));
-        }
         
-        metaBytes.Dequeue(); //Consume end
+        private const byte Terminator = 0x65;             //e
+        private const byte StringLengthTerminator = 0x3A; //:
 
-        return list;
-    }
-
-    private static Dictionary<string, object> ReadDictionary(Queue<byte> metaBytes)
-    {
-        var dictionary = new Dictionary<string, object>();
-        metaBytes.Dequeue(); //Consume
+        private readonly Stream _stream;
+        private readonly byte[] _buffer = new byte[2048];
         
-        while (PeekNextType(metaBytes) != BEncodingType.End)
+        private int _bufferSize;
+        private int _bufferIndex;
+        private byte _current;
+        
+
+        public BEncodingReader(Stream stream)
         {
-            var key = ReadString(metaBytes);
-            dictionary[key] = ReadValue(metaBytes);
+            _stream = stream;
         }
 
-        metaBytes.Dequeue(); //Consume end
-
-        return dictionary;
-    }
-
-    private static object ReadValue(Queue<byte> metaBytes)
-    {
-        return PeekNextType(metaBytes) switch
+        public object Consume()
         {
-            BEncodingType.List => ReadList(metaBytes),
-            BEncodingType.Dictionary => ReadDictionary(metaBytes),
-            BEncodingType.Integer => ReadInteger(metaBytes),
-            BEncodingType.String => ReadString(metaBytes)
-        };
-    }
-
-    private static BEncodingType PeekNextType(Queue<byte> metaBytes)
-    {
-        return metaBytes.Peek() switch
-        {
-            0x65                        => BEncodingType.End,         //e
-            0x6C                        => BEncodingType.List,        //l
-            0x64                        => BEncodingType.Dictionary,  //d
-            0x69                        => BEncodingType.Integer,     //i
-            byte and > 0x30 and <= 0x39 => BEncodingType.String,      //between 1 and 9
-            _                           => BEncodingType.Unknown
-        };
-    }
-
-    private static int ReadInteger(Queue<byte> metaData)
-    {
-        metaData.Dequeue(); //Consume type marker
-
-        var isNegative = metaData.Peek() == 0x2D;
-        if (isNegative) metaData.Dequeue(); //Skip negative marker
-        
-        var value = ReadInteger(metaData, 0x65);
-        
-        return isNegative ? -value : value;
-    }
-
-    private static int ReadInteger(Queue<byte> metaData, byte terminator)
-    {
-        //877
-        //8 * 10 = 80
-        //+ 7 = 87
-        //87 * 10 = 870
-        //etc.
-        
-        var value = 0;
-        for (var current = metaData.Dequeue(); current != terminator; current = metaData.Dequeue())
-        {
-            value *= 10;
-            value += current - 0x30; //x030 = UTF-8  DIGIT ZERO
+            Advance(); //Advance type token
+            return ReadValue();
         }
-        return value;
-    }
 
-    private static string ReadString(Queue<byte> metaBytes)
-    {
-        var sb = new StringBuilder();
-        var length = ReadInteger(metaBytes, 0x3A);
-        while (length-- > 0)
+        private object ReadValue()
         {
-            sb.Append((char)metaBytes.Dequeue());   
+            return CurrentType() switch
+            {
+                BType.String     => ReadString(),
+                BType.Integer    => ReadInteger(),
+                BType.List       => ReadList(),
+                BType.Dictionary => ReadDictionary(),
+                _                => throw new ArgumentException("Unknown type identifier: " + (char)_current)
+            };
         }
-        return sb.ToString();
+        
+        private void Advance()
+        {
+            //Refill our buffer once exhausted, reset our index to the start
+            if (_bufferIndex == _bufferSize)
+            {
+                _bufferSize = _stream.Read(_buffer, 0, _buffer.Length);
+                _bufferIndex = 0;
+            }
+
+            //Consume from our buffer, move our index to the right by 1 by decrementing our offset
+            _current = _buffer[_bufferIndex++];
+        }
+
+        private bool Advance(byte terminator)
+        {
+            Advance();
+            
+            //For ease, simply check for our general terminator
+            return _current != terminator;
+        }
+
+        private string ReadString()
+        {
+            var sb = new StringBuilder();
+            
+            //Strings are unique in their type token also dubbing as the start of their length indicator
+            var length = ToInt(_current);
+            while (Advance(StringLengthTerminator)) length = length * 10 + ToInt(_current);
+
+            while (length-- > 0)
+            {
+                Advance();
+                sb.Append((char)_current);
+            }
+            
+            return sb.ToString();
+        }
+        
+        private int ReadInteger()
+        {
+            //Keep reading until we hit a terminator - we read from left to right, so multiply each previous value with a factor of 10 to get the proper integer representation
+            //874 ->
+            //8
+            //80 + 7
+            //870 + 4
+            var value = 0;
+            while (Advance(Terminator)) value = value * 10 + ToInt(_current);
+            return value;
+        }
+        
+        private List<object> ReadList()
+        {
+            //Keep reading individual items until we hit a terminator
+            var list = new List<object>();
+            while (Advance(Terminator)) list.Add(ReadValue());
+            return list;
+        }
+        
+        private Dictionary<string, object> ReadDictionary()
+        {
+            //Dictionaries consist of keyvaluepairs with keys always being string values
+            //keep consuming until we hit a terminator, this marks our final item
+            var dictionary = new Dictionary<string, object>();
+            while (Advance(Terminator))
+            {
+                var key = ReadString();
+                Advance(); //Consume next value indicator token
+                var value = ReadValue();
+                dictionary[key] = value;
+            }
+            return dictionary;
+        }
+
+        private BType CurrentType()
+        {
+            if (_current > 0x30 && _current <= 0x39)   return BType.String;     //1 - 9
+            if (_current == 0x69)                      return BType.Integer;    //i
+            if (_current == 0x64)                      return BType.Dictionary; //d
+            if (_current == 0x6C)                      return BType.List;       //l
+            return BType.Unknown;
+        }
+        
+        private int ToInt(byte b) => b - 0x30;
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+
+        private enum BType
+        {
+            Unknown,
+            
+            Dictionary,
+            List,
+            Integer,
+            String
+        }
     }
-}
-
-public class BEncoding
-{
-    private int _metaIndex = 0;
-    private int _typeBytesLeft = 0;
-    private BEncodingType _currentType = BEncodingType.Unknown;
-    private readonly byte[] _bytes;
-
-    public BEncoding(byte[] bytes)
-    {
-        _bytes = bytes;
-    }
-
-    public bool Next()
-    {
-        var nextByte = _bytes[_metaIndex];
-
-        _metaIndex++;
-        return true;
-    }
-}
-
-public enum BEncodingType : byte
-{
-    Unknown,
-    End,
-    
-    Dictionary,
-    List,
-    String,
-    Integer
 }
